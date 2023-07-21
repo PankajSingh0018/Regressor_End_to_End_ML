a5c7a80af23e5cd372b50fe1009c3f2fecf43243
PankajSingh0018 <singh.pankaj0018@gmail.com>
2023-07-21 23:25:05 +0530

    Model Trainer

---
+++ source/pipelines/training_pipeline.py
@@ -0,0 +1,24 @@
+import os 
+import sys 
+import pandas as pd 
+
+# Add the path to the parent directory of the 'source' package folder
+module_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(module_path)
+
+from logger import logging
+from exception import CustomException
+from components.data_ingestion import DataIngestion
+from components.data_transformation import DataTransformation
+from components.model_trainer import ModelTrainer
+
+
+
+if __name__ == "__main__":
+    
+    obj= DataIngestion()
+    train_data_path, test_data_path= obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    train_arr, test_arr,_= data_transformation.initaite_data_transformation(train_data_path,test_data_path)
+    model_trainer =ModelTrainer()
+    model_trainer.initiate_model_trainer( train_arr,test_arr)
--- source/utils.py
+++ source/utils.py
@@ -3,7 +3,6 @@ import sys
 import pandas as pd 
 import numpy as np 
 import pickle
-import os
 
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 
@@ -26,3 +25,37 @@ def save_object( file_path, obj):
     
     except Exception as e:
         raise CustomException(e, sys)
+
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for i in range(len(models)):
+            model_name = list(models.keys())[i]
+            model = list(models.values())[i]
+
+            # Training the model 
+            model.fit(X_train, y_train)
+
+            # Predicting Testing Data
+            y_test_pred = model.predict(X_test)
+
+            # Get the R2score for the  test data 
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score
+
+        return report
+
+    except Exception as e:
+        logging.info("Exception Occured during model training")
+        raise CustomException(e, sys)
+        
+    def load_object(file_path):
+        try:
+            with open(file_path,'rb') as f:
+                return pickle.load(f)
+        
+        except Exception as e:
+            logging.info('Exception Occured in load_object function in utils')
+            raise CustomException(e,sys)
